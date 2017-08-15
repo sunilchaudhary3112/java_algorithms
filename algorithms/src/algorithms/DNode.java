17381acf7e9479b6640cbe1ec84d1ac90d65c22c
Sunil <sunilchaudhary3112@gmail.com>
2017-08-15 20:44:45 +0530

    implemented doubly list

---
+++ algorithms/src/algorithms/DNode.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+public class DNode {
+	int data;
+	DNode prev;
+	DNode next;
+
+	public DNode(int data) {
+		// TODO Auto-generated constructor stub
+		this.data = data;
+		this.prev = null;
+		this.next = null;
+	}
+
+}
--- algorithms/src/algorithms/LinkedList.java
+++ algorithms/src/algorithms/LinkedList.java
@@ -182,119 +182,114 @@ public class LinkedList {
 		}
 		head = prev;
 	}
-	
-	public LNode reverseRecurrsion(LNode current , LNode prev){
-		
-		if(current.next == null){
+
+	public LNode reverseRecurrsion(LNode current, LNode prev) {
+
+		if (current.next == null) {
 			head = current;
 			current.next = prev;
 			return null;
 		}
-		
+
 		LNode next1 = current.next;
-		
+
 		current.next = prev;
-		reverseRecurrsion (next1, current);
+		reverseRecurrsion(next1, current);
 		return head;
 	}
-	
-	public  LNode mergeListsIterative(LNode head1, LNode head2){
-		if(head1 == null){
+
+	public LNode mergeListsIterative(LNode head1, LNode head2) {
+		if (head1 == null) {
 			return head2;
-		}
-		else if(head2 == null){
+		} else if (head2 == null) {
 			return head1;
 		}
 		LNode head3;
-		if(head1.data <= head2.data){
+		if (head1.data <= head2.data) {
 			head3 = head1;
 			head1 = head1.next;
-		}
-		else{
+		} else {
 			head3 = head2;
 			head2 = head2.next;
 		}
 		LNode current3 = head3;
-		while(head1 != null && head2 != null){
-			if(head1.data <= head2.data){
+		while (head1 != null && head2 != null) {
+			if (head1.data <= head2.data) {
 				current3.next = head1;
 				head1 = head1.next;
-			}
-			else{
+			} else {
 				current3.next = head2;
 				head2 = head2.next;
 			}
 			current3 = current3.next;
 		}
-		if(head1 == null){
+		if (head1 == null) {
 			current3.next = head2;
-		}
-		else{
+		} else {
 			current3.next = head1;
 		}
 		return head3;
 	}
-	
-	public LNode mergeListRec(LNode head1 , LNode head2){
+
+	public LNode mergeListRec(LNode head1, LNode head2) {
 		LNode head3;
-		if(head1 == null){
+		if (head1 == null) {
 			return head2;
 		}
-		if(head2 == null){
+		if (head2 == null) {
 			return head1;
 		}
-		
-		if(head1.data <= head2.data){
+
+		if (head1.data <= head2.data) {
 			head3 = head1;
 			head3.next = mergeListRec(head1.next, head2);
-		}
-		else{
+		} else {
 			head3 = head2;
 			head3.next = mergeListRec(head1, head2.next);
 		}
 		return head3;
 	}
 
-	public LNode revereKGoupInList(LNode head1, int k){
+	public LNode revereKGoupInList(LNode head1, int k) {
 		LNode current = head1, prev = null, next = null;
 		int count = 0;
-		while(count < k && current!= null){
+		while (count < k && current != null) {
 			next = current.next;
 			current.next = prev;
 			prev = current;
 			current = next;
 			count++;
 		}
-		
-		if( next!= null){
+
+		if (next != null) {
 			head1.next = revereKGoupInList(next, k);
 		}
 		return prev;
 	}
 
-	public Boolean detectAndRemoveLoop(){
+	public Boolean detectAndRemoveLoop() {
 		LNode slow = head, fast = head;
-		while(slow!= null && fast!= null && fast.next!= null){
+		while (slow != null && fast != null && fast.next != null) {
 			slow = slow.next;
 			fast = fast.next.next;
-			
-			if(slow.data == fast.data){
+
+			if (slow.data == fast.data) {
 				removeLoop(slow);
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public void removeLoop(LNode loopNode){
+
+	public void removeLoop(LNode loopNode) {
 		LNode ptr1 = head, ptr2;
-		while (true){
+		while (true) {
 			ptr2 = loopNode;
-			while(ptr2.next != loopNode && ptr2.next != ptr1){
+			while (ptr2.next != loopNode && ptr2.next != ptr1) {
 				ptr2 = ptr2.next;
 			}
-			
-			if(ptr2.next == ptr1){
+
+			if (ptr2.next == ptr1) {
 				break;
 			}
 			ptr1 = ptr1.next;
@@ -302,31 +297,30 @@ public class LinkedList {
 		ptr2.next = null;
 	}
 
-	public LNode addTwoList(LNode head1, LNode head2){
-		 LNode prev = null, result = null, tempNode;
-		 int carryOver = 0, sum;
-		 while(head1 != null && head2 != null){
-			 
-			 sum = carryOver + head1.data + head2.data;
-			 
-			 carryOver = sum >= 10 ? 1 : 0;
-			 sum = sum % 10;
-			 tempNode = new LNode(sum);
-			 
-			 if(result == null){
-				 result = tempNode;
-			 }
-			 else{
-				 prev.next = tempNode;
-			 }
-			 
-			 prev = tempNode;
-			 head1 = head1.next;
-			 head2 = head2.next;
-		 }
-		 if(carryOver > 0){
-			 prev.next = new LNode(carryOver);
-		 }
-		 return result;
+	public LNode addTwoList(LNode head1, LNode head2) {
+		LNode prev = null, result = null, tempNode;
+		int carryOver = 0, sum;
+		while (head1 != null && head2 != null) {
+
+			sum = carryOver + head1.data + head2.data;
+
+			carryOver = sum >= 10 ? 1 : 0;
+			sum = sum % 10;
+			tempNode = new LNode(sum);
+
+			if (result == null) {
+				result = tempNode;
+			} else {
+				prev.next = tempNode;
+			}
+
+			prev = tempNode;
+			head1 = head1.next;
+			head2 = head2.next;
+		}
+		if (carryOver > 0) {
+			prev.next = new LNode(carryOver);
+		}
+		return result;
 	}
 }
